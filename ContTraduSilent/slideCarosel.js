5d687c509d9e4954ec563fa388e537c54db382ea
Gabriel Martins <gabriel94902002@gmail.com>
2019-07-14 18:30:04 -0300

    Slide do mapa da escola quase concluído.

--- ContTraduSilent/slideCarosel.js
+++ ContTraduSilent/slideCarosel.js
@@ -23,27 +23,14 @@ function slideCarosel() {
 
     folhascontentimgicons.style.left = leftimgicons + "px";
 
-    window.addEventListener("resize", function () {
-        larguraJanela = window.innerWidth;
-        alturaJanela = window.innerHeight;
-        leftimgicons = ((larguraJanela / 2) - left) - ((left * 2) * (contSlides - 1));
-        folhascontentimgicons.style.left = leftimgicons + "px";
-        tamanhoTelaSlide();
-        folhascontentimgicons.style.transition = "";
-        for (let i = 0; i < slidesFolhas.length; i++) {
-            slidesFolhas[i].style.transition = "";
-        }
-    })
+    //_________________________________________Comparar folhas de caderno_______________________________________________
 
-    //----------------------Comparar folhas de caderno-------------------------
     const iconfolhas = [document.getElementById("icontoschool"),
     document.getElementById("icondoghouse")],
         imgparaescola = document.getElementById("imgparaescola"),
         imgcasadecachorro = document.getElementById("imgcasadecachorro");
     let folhavisivel = false;
     mudaSlideFolha();
-    // iconfolhas[contSlides - 1].style.border = "1px solid #d5c06b88";
-    // iconfolhas[contSlides - 1].style.opacity = "1";
 
     compararFolhasCaderno.addEventListener("click", function () {//Visualizar a div para ver as "folhas de caderno de desenho". 
         fadeoutcontentsSlide();
@@ -100,13 +87,11 @@ function slideCarosel() {
     function voltaFolha() {
         if (contSlides > 1) {
             leftimgicons = leftimgicons + left * 2;
-            folhascontentimgicons.style.left = leftimgicons + "px";
             contSlides--;
             mudaSlideFolha();
         }
         else if (contSlides == 1) {
             leftimgicons = ((larguraJanela / 2) - left) - ((left * 2) * (iconfolhas.length - 1));
-            folhascontentimgicons.style.left = leftimgicons + "px";
             contSlides = iconfolhas.length;
             mudaSlideFolha();
         }
@@ -126,6 +111,8 @@ function slideCarosel() {
     };
 
     function mudaSlideFolha() {
+        folhasbtnLeft.removeEventListener("click", voltaFolha);
+        folhasbtnRight.removeEventListener("click", avancaFolha);
         folhascontentimgicons.style.transition = "left 350ms ease-in-out";
         for (let i = 0; i < slidesFolhas.length; i++) {
             slidesFolhas[i].style.transition = "width 350ms ease-in-out";
@@ -136,7 +123,7 @@ function slideCarosel() {
                     slidesFolhas[i].style.width = "0%";
                 }
             }
-            slidesFolhas[contSlides - 1].style.width = "99.9%";
+            slidesFolhas[contSlides - 1].style.width = "100%";
             folhascontentimgicons.style.left = leftimgicons + "px";
             iconfolhas[contSlides - 1].style.border = "1px solid rgba(213, 192, 107, 0.555)";
             setTimeout(function () {
@@ -149,10 +136,132 @@ function slideCarosel() {
                 }
                 imgparaescola.style.opacity = "0";
                 imgcasadecachorro.style.opacity = "0";
+                folhasbtnLeft.addEventListener("click", voltaFolha);
+                folhasbtnRight.addEventListener("click", avancaFolha);
                 folhavisivel = false;
             }, 350)
         }, 5)
-    }
+    };
+    //__________________________________________Comparar Mapa da escola____________________________________________
+
+    const compararescola = document.getElementById("compararEscola"),
+        fundomapaescola = document.getElementById("fundomapaescola"),
+        fecharfundoescola = document.getElementById("fecharfundoescola"),
+        escolacontentimgicons = document.getElementById("escolacontentimgicons"),
+        mapaescolabtnLeft = document.getElementById("mapaescolabtnLeft"),
+        mapaescolabtnRight = document.getElementById("mapaescolabtnRight");
+
+    const iconMapaEscola = [document.getElementById("iconmapaescolap"),
+    document.getElementById("iconmapaescola1a"),
+    document.getElementById("iconmapaescola2a"),
+    document.getElementById("iconmapaescolat")];
+
+    const slidesMapaEscola = [document.getElementById("imgschoolp"),
+    document.getElementById("imgschool1a"),
+    document.getElementById("imgschool2a"),
+    document.getElementById("imgschoolt")];
+
+    escolacontentimgicons.style.left = leftimgicons + "px";
+
+    mudaSlideMapaEscola();
+
+    compararescola.addEventListener("click", function () {//Visualizar a div para comparar o mapa de Escola.
+        fadeoutcontentsSlide();
+        $(fundomapaescola).fadeIn(700);
+        tamanhoTelaSlide();
+    });
+
+    fecharfundoescola.addEventListener("click", function () {//Fechar a div para comparar o mapa de Escola.
+        $(fundomapaescola).fadeOut(300);
+        fadeincontentsSlide();
+        folhavisivel = false;
+        voltaPrimeiroSlideMapaEscola();
+    });
+
+    mapaescolabtnLeft.addEventListener("click", voltaMapaEscola);
+    mapaescolabtnRight.addEventListener("click", avancaMapaEscola);
+
+    iconMapaEscola[0].addEventListener("click", voltaPrimeiroSlideMapaEscola);
+    function voltaPrimeiroSlideMapaEscola() {
+        leftimgicons = ((larguraJanela / 2) - left) - ((left * 2) * (0));
+        contSlides = 1;
+        mudaSlideMapaEscola();
+    };
+
+    iconMapaEscola[1].addEventListener("click", function () {
+        leftimgicons = ((larguraJanela / 2) - left) - ((left * 2) * (1));
+        contSlides = 2;
+        mudaSlideMapaEscola();
+    });
+
+    iconMapaEscola[2].addEventListener("click", function () {
+        leftimgicons = ((larguraJanela / 2) - left) - ((left * 2) * (2));
+        contSlides = 3;
+        mudaSlideMapaEscola();
+    });
+
+    iconMapaEscola[3].addEventListener("click", function () {
+        leftimgicons = ((larguraJanela / 2) - left) - ((left * 2) * (3));
+        contSlides = 4;
+        mudaSlideMapaEscola();
+    });
+
+    function voltaMapaEscola() {
+        if (contSlides > 1) {
+            leftimgicons = leftimgicons + left * 2;
+            contSlides--;
+            mudaSlideMapaEscola();
+        }
+        else if (contSlides == 1) {
+            leftimgicons = ((larguraJanela / 2) - left) - ((left * 2) * (iconMapaEscola.length - 1));
+            contSlides = iconMapaEscola.length;
+            mudaSlideMapaEscola();
+        }
+    };
+
+    function avancaMapaEscola() {
+        if (contSlides < iconMapaEscola.length) {
+            leftimgicons = leftimgicons - left * 2;
+            contSlides++;
+            mudaSlideMapaEscola();
+        }
+        else if (contSlides == iconMapaEscola.length) {
+            leftimgicons = (larguraJanela / 2) - left;
+            contSlides = 1;
+            mudaSlideMapaEscola();
+        }
+    };
+
+    function mudaSlideMapaEscola() {
+        mapaescolabtnLeft.removeEventListener("click", voltaMapaEscola);
+        mapaescolabtnRight.removeEventListener("click", avancaMapaEscola);
+        escolacontentimgicons.style.transition = "left 350ms ease-in-out";
+        for (let i = 0; i < slidesMapaEscola.length; i++) {
+            slidesMapaEscola[i].style.transition = "width 350ms ease-in-out";
+        }
+        setTimeout(function () {
+            for (let i = 0; i < slidesMapaEscola.length; i++) {
+                if (i != (contSlides - 1)) {
+                    slidesMapaEscola[i].style.width = "0%";
+                }
+            }
+            slidesMapaEscola[contSlides - 1].style.width = "100%";
+            escolacontentimgicons.style.left = leftimgicons + "px";
+            iconMapaEscola[contSlides - 1].style.border = "1px solid rgba(213, 192, 107, 0.555)";
+            setTimeout(function () {
+                iconMapaEscola[contSlides - 1].style.opacity = "1";
+                for (let i = 0; i < iconMapaEscola.length; i++) {
+                    if (i != contSlides - 1) {
+                        iconMapaEscola[i].style.border = "";
+                        iconMapaEscola[i].style.opacity = "";
+                    }
+                }
+                mapaescolabtnLeft.addEventListener("click", voltaMapaEscola);
+                mapaescolabtnRight.addEventListener("click", avancaMapaEscola);
+                folhavisivel = false;
+            }, 350)
+        }, 5)
+    };
 
     function tamanhoTelaSlide() {
         for (let i = 0; i < telaslide.length; i++) {
@@ -169,7 +278,7 @@ function slideCarosel() {
                 slides[i].style.height = slides[i].offsetWidth / proporcao4por3 + "px";
             }
         }
-    }
+    };
 
     //____________________________________________________________________________________________________________________
 
@@ -178,13 +287,30 @@ function slideCarosel() {
         for (let i = 0; i < contents.length; i++) {
             $(contents[i]).fadeIn(700);
         }
-    }
+    };
 
     function fadeoutcontentsSlide() {//Faz todas as divs que possuem conteúdo "aparecerem".       
         $(bttverdetalhes).fadeOut(300);
         for (let i = 0; i < contents.length; i++) {
             $(contents[i]).fadeOut(300);
         }
-    }
+    };
+
+    window.addEventListener("resize", function () {
+        larguraJanela = window.innerWidth;
+        alturaJanela = window.innerHeight;
+        leftimgicons = ((larguraJanela / 2) - left) - ((left * 2) * (contSlides - 1));
+        folhascontentimgicons.style.left = leftimgicons + "px";
+        escolacontentimgicons.style.left = leftimgicons + "px";
+        tamanhoTelaSlide();
+        folhascontentimgicons.style.transition = "";
+        escolacontentimgicons.style.transition = "";
+        for (let i = 0; i < slidesFolhas.length; i++) {
+            slidesFolhas[i].style.transition = "";
+        }
+        for (let i = 0; i < slidesMapaEscola.length; i++) {
+            slidesMapaEscola[i].style.transition = "";
+        }
+    });
 }
 
