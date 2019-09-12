2411b4ca68046c93e6898dc639c0100093635540
Gabriel Martins <gabriel94902002@gmail.com>
2019-09-12 09:08:21 -0300

    Pesquisa de cor por código RGB quase concluído.

--- colorPaint/colorPaint.js
+++ colorPaint/colorPaint.js
@@ -17,14 +17,27 @@ function colorPaint() {
 
     let corPuraVarrer = { R: 255, G: 0, B: 0 };
 
+    let corParaAchar = {};
+    let hsvBarra = { H: 0, S: 100, V: 100 };
+
     preencheBarraEspectro();
     preencheGradiente();
 
-    let corParaachar;
 
     codRGB.addEventListener("keyup", function (e) {
-        corParaachar = this.value;
-        encontrarCorDoCodigoNoGradiente(corParaachar);
+        let codCorAchar = this.value;
+        codCorAchar = codCorAchar.split(",") || codCorAchar.split(", ");
+        for (let i = 0; i < codCorAchar.length; i++) {
+            codCorAchar[i] = parseInt(codCorAchar[i]);//Converte as STRINGS em INT.
+        }
+        if (codCorAchar.length === 3) {
+            if (codCorAchar[0] <= 255 && codCorAchar[1] <= 255 && codCorAchar[2] <= 255) {
+                corParaAchar = { R: codCorAchar[0], G: codCorAchar[1], B: codCorAchar[2] }
+                hsvBarra = rgbToHsv(corParaAchar);
+                corPuraVarrer = hsvToRgb(hsvBarra.H, 100, 100);
+                encontrarCorDoCodigoNoGradiente(hsvBarra.S, hsvBarra.V);
+            }
+        }
     });
 
     let clickBarra = false;
@@ -53,87 +66,36 @@ function colorPaint() {
                 mouseX = e.layerX;
             }
             let data = ctxBarra.getImageData((mouseX * 3), mouseY, 1, 1).data;
-            let codCor = {R: data[0], G: data[1], B: data[2]};
+            let codCor = { R: data[0], G: data[1], B: data[2] };
+            hsvBarra = rgbToHsv(codCor);
             encontrarCorDoCodigoNaBarra(codCor);
         };
     }
 
-    function encontrarCorDoCodigoNoGradiente(cod) {
-        let codColor = cod.split(",") || cod.split(", ");//Separa os  números da string do código RGB num array.
-        for (let i = 0; i < codColor.length; i++) {
-            codColor[i] = parseInt(codColor[i]);//Converte as STRINGS dos números em INT.
-        }
-
-        if (codColor[0] <= 255 && codColor[1] <= 255 && codColor[2] <= 255 && codColor.length === 3) {//Antes de procurar verifica se o código RGB está correto.
-            let conteudoGradiente = ctxGradiente.getImageData(0, 0, widthGradiente, heightGradiente);
-            let data = conteudoGradiente.data;
-            let achouacor = false;
-            for (let i = 0; i < data.length; i += 4) {//Verifica a cor de pixel a pixel do canvas (gradienteCor) para comparar com o cod.
-                let R = data[i], G = data[i + 1], B = data[i + 2];
-
-                if (R === codColor[0] && G === codColor[1] && B === codColor[2]) {//Verifica se as cores são iguais.
-                    let x = i / 4 % widthGradiente, y = (i / 4 - x) / widthGradiente;//Calcula a coordenada do pixel daquela cor.
-                    let codPixel = R + ", " + G + ", " + B;//
-                    moverCursorGradiente((x / 2) - 9, (y / 2) - 10, codPixel);
-                    encontrarCorDoCodigoNaBarra(corPuraVarrer);
-                    i = data.length;
-                    achouacor = true;
-                }
-                
-            }
-            if (achouacor === false) {
-                varrerCoresPuras(corPuraVarrer);
-                preencheGradiente();
-                encontrarCorDoCodigoNoGradiente(corParaachar);
-            }
-            conteudoGradiente = null;
-            data = null;
-        }
-    }
-
-    function varrerCoresPuras(cor) {
-        if (cor.R === 255 && cor.G >= 0 && cor.G < 255 && cor.B === 0) {
-            corPuraVarrer.G += 1;
-        }
-        else if (cor.R > 0 && cor.R <= 255 && cor.G === 255 && cor.B === 0) {
-            corPuraVarrer.R -= 1;
-        }
-        else if (cor.R === 0 && cor.G === 255 && cor.B >= 0 && cor.B < 255) {
-            corPuraVarrer.B += 1;
-        }
-        else if (cor.R === 0 && cor.G <= 255 && cor.G > 0 && cor.B === 255) {
-            corPuraVarrer.G -= 1;
-        }
-        else if (cor.R >= 0 && cor.R < 255 && cor.G === 0 && cor.B === 255) {
-            corPuraVarrer.R += 1;
-        }
-        else if (cor.R === 255 && cor.G === 0 && cor.B <= 255 && cor.B > 0) {
-            corPuraVarrer.B -= 1;
-        }
+    function encontrarCorDoCodigoNoGradiente(s, v) {
+        preencheGradiente();
+        let posx = (gradienteCor.offsetWidth / 100) * s;
+        let posy = gradienteCor.offsetHeight - ((gradienteCor.offsetHeight / 100) * v);
+        moverCursorGradiente(posx - 10, posy - 10, corParaAchar);
+        encontrarCorDoCodigoNaBarra(corPuraVarrer);
     }
 
     function encontrarCorDoCodigoNaBarra(cod) {
-        // let codColor = cod.split(",") || cod.split(", ");//Separa os  números da string do código RGB num array.
-        // for (let i = 0; i < codColor.length; i++) {
-        //     codColor[i] = parseInt(codColor[i]);//Converte as STRINGS dos números em INT.
-        // }
-        if (cod.R <= 255 && cod.G <= 255 && cod.B <= 255) {//Antes de procurar verifica se o código RGB está correto.
-            let conteudoBarra = ctxBarra.getImageData(0, 0, widthBarra, 1);
-            let data = conteudoBarra.data;
-            for (let i = 0; i < data.length; i += 4) {//Verifica a cor de pixel a pixel do canvas (gradienteCor) para comparar com o cod.
-                let R = data[i], G = data[i + 1], B = data[i + 2];
-                if (R === cod.R && G === cod.G && B === cod.B) {//Verifica se as cores são iguais.
-                    let x = i / 4 % widthBarra;//Calcula a coordenada do pixel daquela cor.
-                    let codPixel = {R: R, G: G, B: B};//
-                    moverCursorBarra(x / 3, codPixel);
-                    i = data.length;
-                }
+        let conteudoBarra = ctxBarra.getImageData(0, 0, widthBarra, 1);
+        let data = conteudoBarra.data;
+        for (let i = 0; i < data.length; i += 4) {//Verifica a cor de pixel a pixel do canvas (gradienteCor) para comparar com o cod.
+            let R = data[i], G = data[i + 1], B = data[i + 2];
+            if (R === cod.R && G === cod.G && B === cod.B) {//Verifica se as cores são iguais.
+                let x = i / 4 % widthBarra;//Calcula a coordenada do pixel daquela cor.
+                let codPixel = { R: R, G: G, B: B };//
+                moverCursorBarra(x / 3, codPixel);
+                i = data.length;
             }
         }
     }
 
     function moverCursorGradiente(x, y, cod) {
-        let corSelecionadaGradiente = "rgb(" + cod + ")"
+        let corSelecionadaGradiente = "rgb(" + cod.R + ", " + cod.G + ", " + cod.B + ")"
         cursorGradiente.style.left = x + "px";
         cursorGradiente.style.top = y + "px";
         cursorGradiente.style.backgroundColor = corSelecionadaGradiente;
@@ -143,14 +105,13 @@ function colorPaint() {
     function moverCursorBarra(x, cor) {
         corPuraVarrer = cor;
         cursorBarra.style.left = x + "px";
-        cursorBarra.style.backgroundColor = "rgb(" + corPuraVarrer.R + ", "+ corPuraVarrer.G + ", " + corPuraVarrer.B + ")";
+        cursorBarra.style.backgroundColor = "rgb(" + corPuraVarrer.R + ", " + corPuraVarrer.G + ", " + corPuraVarrer.B + ")";
         preencheGradiente();
     }
 
     function preencheBarraEspectro() {
         ctxBarra.rect(0, 0, widthBarra, heightBarra);
         let gradiente = ctxBarra.createLinearGradient(0, 0, widthBarra, 0);
-
         gradiente.addColorStop(0, "rgb(255, 0, 0)");
         gradiente.addColorStop(0.16666666666666666667, "rgb(255, 255, 0)");
         gradiente.addColorStop(0.33333333333333333334, "rgb(0, 255, 0)");
@@ -164,7 +125,7 @@ function colorPaint() {
 
     function preencheGradiente() {
         ctxGradiente.clearRect(0, 0, widthGradiente, heightGradiente);
-        ctxGradiente.fillStyle = "rgb(" + corPuraVarrer.R + ", "+ corPuraVarrer.G + ", " + corPuraVarrer.B + ")";
+        ctxGradiente.fillStyle = "rgb(" + corPuraVarrer.R + ", " + corPuraVarrer.G + ", " + corPuraVarrer.B + ")";
         ctxGradiente.fillRect(0, 0, widthGradiente, heightGradiente);
 
         let gradienteBranco = ctxBarra.createLinearGradient(0, 0, widthGradiente, 0);
@@ -180,17 +141,122 @@ function colorPaint() {
         ctxGradiente.fillRect(0, 0, widthGradiente, heightGradiente);
         ctxGradiente.closePath();
 
-        let conteudoGradiente = ctxGradiente.getImageData((cursorGradiente.offsetLeft + 9) * 2, (cursorGradiente.offsetTop + 10) * 2, 1, 1);
-        let data = conteudoGradiente.data;
-        let cor = data[0] + ", " + data[1] + ", " + data[2];
-        cursorGradiente.style.backgroundColor = "rgb(" + cor + ")";
-        corSelecionada.style.backgroundColor = "rgb(" + cor + ")";
+        let S = (gradienteCor.offsetWidth - (gradienteCor.offsetWidth - (cursorGradiente.offsetLeft + 10))) * (100 / gradienteCor.offsetWidth);
+        let V = 100 - (gradienteCor.offsetHeight - (gradienteCor.offsetHeight - (cursorGradiente.offsetTop + 10))) * (100 / gradienteCor.offsetHeight);
+        if(S == 0){
+            S = 0.1;
+        }
+        if(V == 0){
+            V = 0.1;
+        }
+        let cor = hsvToRgb(hsvBarra.H, S, V);
+        cursorGradiente.style.backgroundColor = "rgb(" + cor.R + ", " + cor.G + ", " + cor.B + ")";
+        corSelecionada.style.backgroundColor = "rgb(" + cor.R + ", " + cor.G + ", " + cor.B + ")";
+        codRGB.value = cor.R + ", " + cor.G + ", " + cor.B;
     }
 
-    // while(corPuraVarrer.R != 255 || corPuraVarrer.G != 0 || corPuraVarrer.B != 0 ){
-    //     varrerCoresPuras(corPuraVarrer);
-    //     console.log(corPuraVarrer.R + ", " + corPuraVarrer.G + ", " + corPuraVarrer.B);
-    // }
-    
+    function rgbToHsv(cor) {
+        let rabs, gabs, babs, rr, gg, bb, h, v, diff, diffc, percentRoundFn;
+        rabs = cor.R / 255;
+        gabs = cor.G / 255;
+        babs = cor.B / 255;
+        v = Math.max(rabs, gabs, babs),
+            diff = v - Math.min(rabs, gabs, babs);
+        diffc = c => (v - c) / 6 / diff + 1 / 2;
+        percentRoundFn = num => (num * 100) / 100;
+        if (diff == 0) {
+            h = s = 0;
+        } else {
+            s = diff / v;
+            rr = diffc(rabs);
+            gg = diffc(gabs);
+            bb = diffc(babs);
+
+            if (rabs === v) {
+                h = bb - gg;
+            } else if (gabs === v) {
+                h = (1 / 3) + rr - bb;
+            } else if (babs === v) {
+                h = (2 / 3) + gg - rr;
+            }
+            if (h < 0) {
+                h += 1;
+            } else if (h > 1) {
+                h -= 1;
+            }
+        }
+        return {
+            H: (h * 360),
+            S: percentRoundFn(s * 100),
+            V: percentRoundFn(v * 100)
+        };
+    }
+
+    function hsvToRgb(h, s, v) {
+        var r, g, b;
+        var i;
+        var f, p, q, t;
+
+        h = Math.max(0, Math.min(360, h));
+        s = Math.max(0, Math.min(100, s));
+        v = Math.max(0, Math.min(100, v));
+
+        s /= 100;
+        v /= 100;
+
+        if (s == 0) {
+            // Achromatic (grey)
+            r = g = b = v;
+            return [
+                Math.round(r * 255),
+                Math.round(g * 255),
+                Math.round(b * 255)
+            ];
+        }
+
+        h /= 60; // sector 0 to 5
+        i = Math.floor(h);
+        f = h - i; // factorial part of h
+        p = v * (1 - s);
+        q = v * (1 - s * f);
+        t = v * (1 - s * (1 - f));
+
+        switch (i) {
+            case 0:
+                r = v;
+                g = t;
+                b = p;
+                break;
+            case 1:
+                r = q;
+                g = v;
+                b = p;
+                break;
+            case 2:
+                r = p;
+                g = v;
+                b = t;
+                break;
+            case 3:
+                r = p;
+                g = q;
+                b = v;
+                break;
+            case 4:
+                r = t;
+                g = p;
+                b = v;
+                break;
+            case 5:
+                r = v;
+                g = p;
+                b = q;
+        }
+        return {
+            R: Math.round(r * 255),
+            G: Math.round(g * 255),
+            B: Math.round(b * 255)
+        };
+    }
 }
 
